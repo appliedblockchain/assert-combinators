f1a0607a222f4c4dd19d14ada3f41e9ab41b2616
Mirek Rusin <mirek@me.com>
2020-04-13 11:29:16 +0200

    Don't collide with mixed.

--- array.js
+++ array.js
@@ -1,15 +1,15 @@
 // @flow strict
 
-const mixed = require('./mixed')
+const mixed_ = require('./mixed')
 const rethrow = require('./helpers/rethrow')
 
 const array /*: <T>(a?: mixed => T) => (mixed => T[]) */ = /*:: <T> */
-  (a = mixed) =>
+  (a = mixed_) =>
     value => {
       if (!Array.isArray(value)) {
         throw new TypeError('Expected array.')
       }
-      if (a && a !== mixed) {
+      if (a && a !== mixed_) {
         value.forEach((_, i) => {
           try {
             a(_)
