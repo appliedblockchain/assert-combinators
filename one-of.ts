91f1ab68de24a69d46b68bea257d987cfe486d7a
Mirek Rusin <mirek@me.com>
2021-06-15 23:07:14 +0200

    Assert primitive type union in one-of.

--- one-of.ts
+++ one-of.ts
@@ -1,16 +1,28 @@
 import type Assert from './types/assert'
 import { inspect } from 'util'
 
+/**
+ * Asserts primitive type union.
+ *
+ * @example
+ *   import * as $ from '@appliedblockchain/assert-combinators'
+ *   export const assert = $.oneOf('A', 'B', 'C')
+ *   export type T = $.Asserted<typeof assert>
+ *   // type T is 'A' | 'B' | 'C'
+ *
+ */
 const oneOf =
-  <T extends unknown[]>(...values: T): Assert<T[number]> => {
+  <T extends undefined | null | boolean | number | string | symbol>(...values: readonly T[]): Assert<T> => {
     if (!values.length) {
       throw new TypeError('Expected non-zero arity.')
     }
     return (value: unknown) => {
-      if (!values.includes(value)) {
-        throw new TypeError(`Expected ${inspect(value)} to be one of ${inspect(values)}.`)
+      for (const value_ of values) {
+        if (value_ === value) {
+          return value as T
+        }
       }
-      return value
+      throw new TypeError(`Expected ${inspect(value)} to be one of ${inspect(values)}.`)
     }
   }
 
