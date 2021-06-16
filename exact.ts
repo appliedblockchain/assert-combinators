453e9092b87e6c9fbd4ec6d04d211fd49dfe8db1
Mirek Rusin <mirek@me.com>
2021-06-16 09:13:00 +0200

    Simplifying exact types.

--- exact.ts
+++ exact.ts
@@ -1,32 +1,22 @@
 import type Assert from './types/assert'
+import type Asserted from './types/asserted'
 import { inspect } from 'util'
 import object from './object'
 
-type Result<T> = {
-  [K in keyof T]: T[K] extends Assert<unknown> ?
-  ReturnType<T[K]> :
-  never
-}
-
 const exact =
-  <T extends { [ key: string ]: Assert<unknown> }>(kvs: T): Assert<{
-    [ K in keyof T ]: T[ K ] extends Assert<unknown> ?
-      ReturnType<T[ K ]> :
-      never
-  }> =>
+  <T extends Record<string, Assert<unknown>>>(kvs: T): Assert<{ [k in keyof T]: Asserted<T[k]> }> =>
     value => {
-      if (value === null || typeof value !== 'object') {
+      if (typeof value !== 'object' || value === null) {
         throw new TypeError(`Expected object, got ${inspect(value)}.`)
       }
-      const value_ = value as { [key: string]: unknown }
-      const result = object(kvs)(value_)
+      const result = object(kvs)(value)
       const keys = Object
-        .keys(value_)
+        .keys(value)
         .filter(key => !Object.prototype.hasOwnProperty.call(kvs, key))
       if (keys.length) {
-        throw new TypeError(`Unexpected extra keys ${keys.map(String).join(', ')} in ${inspect(value_)}.`)
+        throw new TypeError(`Unexpected extra keys ${keys.map(String).join(', ')} in ${inspect(value)}.`)
       }
-      return result as Result<T>
+      return result as { [k in keyof T]: Asserted<T[k]> }
     }
 
 export default exact
